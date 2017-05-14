accb7a39c29023f6f3940b059122aa6f5d203e3a
Jessy Lawrence <dradaxia@gmail.com>
2017-05-14 06:54:32 +0000

    Création model Room

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '',
-                    :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
+                    :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profil'},
                     :controllers => {:registrations => 'registrations'}
   
   # The priority is based upon order of creation: first created -> highest priority.
