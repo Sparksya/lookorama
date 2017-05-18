86a7c8894601d6452c20f2aa9b5264eed0601f84
Jessy Lawrence <dradaxia@gmail.com>
2017-05-18 10:51:29 +0000

    Création controller Review

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :rooms, path: 'annonces' do 
-  resources :reservations, only: [:create]
+    resources :reservations, only: [:create]
+    resources :reviews, only: [:create, :destroy]
   end
   resources :photos
 
