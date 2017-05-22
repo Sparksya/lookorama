4272e953d51934c416a6e778a001a770265703f2
Jessy Lawrence <dradaxia@gmail.com>
2017-05-22 07:46:59 +0000

    Remplacement rectification erreur def show

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -28,12 +28,11 @@ before_action :require_same_user, only: [:edit, :update]
     end
    
     def show
-        @photos = @room.photos
-        
-        @booked = Reservation.where("room_id = ? AND user_id = ?", @room.id, current_user.id).present? 
+        @reviews = @room.reviews
+
         if current_user
-            @reviews = @room.reviews
-            @hasReview = @reviews.find_by(user_id: current_user.id)
+          @booked = Reservation.where("room_id = ? AND user_id = ?", @room.id, current_user.id).present? 
+          @hasReview = @reviews.find_by(user_id: current_user.id)
         end
     end
     
