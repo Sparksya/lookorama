5448c697a4d620942e817c677f496602c9c1490d
Jessy Lawrence <dradaxia@gmail.com>
2017-05-25 06:31:41 +0000

    Création model payment

--- Gemfile
+++ Gemfile
@@ -37,8 +37,9 @@ gem 'ransack'
 gem 'rails-timeago'
 
 gem 'omniauth'
-
 gem 'omniauth-facebook'
+
+gem 'stripe'
 
 
 # Use jquery as the JavaScript library
---
+++ config/stripe.rb
@@ -0,0 +1,7 @@
+Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_5tyBvNG7C11rlq5D8FGBQ5o2',
+    :secret_key      => 'sk_test_GXtxPvoGzCXXFf7TxQRsisnD'
+ 
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
